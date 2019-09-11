d071a690271e243fd55cc74cdf3208d54c06e62f
Oliver <oliver@secondwindconsultants.com>
2019-09-11 16:10:57 -0400

    Heroku v2.6

--- bestrestapi/urls.py
+++ bestrestapi/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 from .api import router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(router.urls)),
+    path('', include(router.urls)),
 ]
